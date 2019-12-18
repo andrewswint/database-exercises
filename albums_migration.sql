f54f4d958eb94140a3559bbdcdf53a19925d48fc
andrewswint <andrewswint1976@gmail.com>
2019-12-18 10:55:06 -0600

    finished part one

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,9 +1,10 @@
 USE codeup_test_db;
-CREATE TABLE albums (
+DROP TABLE albums;
+CREATE TABLE IF NOT EXISTS albums (
 id INT UNSIGNED NOT NULL AUTO_INCREMENT,
 artist VARCHAR(50) NOT NULL,
 name VARCHAR(50) NOT NULL,
-release_date DATE,
+release_date INT,
 sales FLOAT UNSIGNED NOT NULL,
 genre VARCHAR(50) NOT NULL,
 PRIMARY KEY (id)
