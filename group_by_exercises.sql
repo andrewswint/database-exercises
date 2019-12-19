6f4e375eeadc52dcdaff2deaf2ca41c227b940c0
andrewswint <andrewswint1976@gmail.com>
2019-12-19 11:56:50 -0600

    aliases exercise complete

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -6,15 +6,13 @@ SELECT DISTINCT title FROM titles;
 # Find your query for employees whose last names start and end with 'E'. Update the query to find just the unique last names that start and end with 'E' using GROUP BY.
 SELECT last_name
 FROM employees
-WHERE last_name LIKE ('E%')
-  AND last_name LIKE '%e'
+WHERE last_name LIKE 'E%e'
 GROUP BY last_name;
 
 # Update your previous query to now find unique combinations of first and last name where the last name starts and ends with 'E'. You should get 846 rows.
 SELECT last_name, first_name
 FROM employees
-WHERE last_name LIKE ('E%')
-  AND last_name LIKE '%e'
+WHERE last_name LIKE 'E%e'
 GROUP BY last_name, first_name;
 
 # Find the unique last names with a 'q' but not 'qu'.
