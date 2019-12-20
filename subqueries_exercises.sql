50ac5164052e0f6d46a006ae4dd745978bd7addf
andrewswint <andrewswint1976@gmail.com>
2019-12-20 14:27:42 -0600

    bonus work

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -22,9 +22,58 @@ WHERE emp_no IN (
 
 # Find all the current department managers that are female.
 SELECT first_name, last_name
-    FROM employees
+FROM employees
+WHERE emp_no IN (
+    SELECT emp_no
+    FROM dept_manager
+    WHERE gender = 'f'
+      AND to_date > now()
+);
+
+# Find all the department names that currently have female managers.
+SELECT dept_name
+FROM departments
+WHERE dept_no IN (
+    SELECT dept_no
+    FROM dept_manager
     WHERE emp_no IN (
         SELECT emp_no
-        FROM dept_manager
-        WHERE gender = 'f' AND to_date > now()
-        );
+        FROM employees
+        WHERE to_date > now()
+          AND gender = 'f'
+    )
+);
+
+# Find the first and last name of the employee with the highest salary.
+SELECT first_name, last_name
+FROM employees
+WHERE emp_no = (
+    SELECT emp_no
+    FROM salaries
+    ORDER BY salary DESC
+    LIMIT 1
+);
+
+# Concat the max salaries of the first two employees
+SELECT CONCAT((SELECT MAX(salary)
+               FROM salaries
+               WHERE emp_no = (
+                   SELECT emp_no
+                   FROM employees
+                   GROUP BY emp_no
+                   ORDER BY emp_no
+                   LIMIT 1
+               )
+              ), ' ',
+    (SELECT MAX(salary)
+     FROM salaries
+     WHERE emp_no = (
+         SELECT emp_no
+         FROM employees
+         GROUP BY emp_no
+         ORDER BY emp_no
+         LIMIT 1 OFFSET 1
+        )
+           )
+    )
+
